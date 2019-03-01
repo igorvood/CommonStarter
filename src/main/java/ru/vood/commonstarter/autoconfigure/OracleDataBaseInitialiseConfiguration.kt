426befa3293515bcb16439106a43c29a469da5a2
balitsky_igor <balitsky_igor@list.ru>
2019-03-01 19:29:42 +0300

    initial

---
+++ src/main/java/ru/vood/commonstarter/autoconfigure/OracleDataBaseInitialiseConfiguration.kt
@@ -0,0 +1,10 @@
+package ru.vood.commonstarter.autoconfigure
+
+import org.springframework.context.annotation.Bean
+
+//@Configuration
+open class OracleDataBaseInitialiseConfiguration {
+
+    @Bean
+    fun sdfsf() = "dfsf"
+}
--- src/main/java/ru/vood/commonstarter/environmentPostProcessors/DeveloperProfilerResolverEnvironmentPostProcessor.kt
+++ src/main/java/ru/vood/commonstarter/environmentPostProcessors/DeveloperProfilerResolverEnvironmentPostProcessor.kt
@@ -1,14 +1,24 @@
 package ru.vood.commonstarter.environmentPostProcessors
 
+import org.apache.commons.logging.LogFactory
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.diagnostics.LoggingFailureAnalysisReporter
 import org.springframework.boot.env.EnvironmentPostProcessor
 import org.springframework.core.env.ConfigurableEnvironment
 
 class DeveloperProfilerResolverEnvironmentPostProcessor : EnvironmentPostProcessor {
+
+    private val log = LogFactory.getLog(LoggingFailureAnalysisReporter::class.java)
+
     override fun postProcessEnvironment(environment: ConfigurableEnvironment?, application: SpringApplication?) {
         if (environment != null && environment.activeProfiles.isEmpty()) {
             val get = System.getenv()["OS"]
-            if (get != null && get.contains("Windows")) environment.addActiveProfile("DEV")
+            var windows: String = "Windows"
+            if (get != null && get.contains(windows)) {
+                var s: String = "DEV"
+                log.debug("NO ACTIVE PROFILE!!! Operation system is $windows, then active profile is $s")
+                environment.addActiveProfile(s)
+            }
         }
     }
 
